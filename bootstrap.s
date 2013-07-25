7519529c06548990c65e677326244662f5c85bb8
Breckin Loggins <bloggins@greatfoundry.com>
2013-07-24 20:44:45 -0700

    From nothing...

---
+++ bootstrap.s
@@ -0,0 +1,34 @@
+	.arch armv7-a
+	.fpu vfpv3
+	.text
+	.global print_uart0
+print_uart0:
+	@ This routine allows us to print onto UART0DR
+	@ We use r0 as the mem address for our char *
+	ldrb	r3, [r0, #0]
+	cmp		r3, #0
+	bxeq	lr
+	ldr 	r2, =UART0DR
+	ldr 	r2, [r2, #0]
+print_uart0_loop:
+	str		r3, [r2, #0]
+	ldrb	r3, [r0, #1]!
+	cmp		r3, #0
+	bne		print_uart0_loop
+	bx 		lr
+
+	.global c_entry
+	@ This is the main entry point from startup.s
+c_entry:
+	ldr 	r0, =HELLO_WORLD
+	bl 		print_uart0
+	b 		.
+
+	.section	.rodata
+	.global UART0DR
+UART0DR:
+	.word 	0x101F1000	@ This is the UART0 address on a versatileboard
+	.section 	.rodata.str1.4, "aMS",%progbits,1
+
+HELLO_WORLD: @ The string to print
+	.ascii	"Nihilo\012\000"
---
+++ nihilo.sh
@@ -0,0 +1,11 @@
+TARGET=/Users/bloggins/bin/ndk/toolchains/arm-linux-androideabi-4.7/prebuilt/darwin-x86_64/bin/arm-linux-androideabi
+$TARGET-as -march=armv7-a startup.s -o startup.o
+$TARGET-as -march=armv7-a bootstrap.s -o bootstrap.o
+$TARGET-ld -T bootstrap.ld startup.o bootstrap.o -o bootstrap.elf
+$TARGET-objcopy -O binary bootstrap.elf bootstrap.bin
+
+rm bootstrap.elf
+rm startup.o
+rm bootstrap.o
+
+qemu-system-arm -machine versatilepb -nographic -kernel bootstrap.bin
