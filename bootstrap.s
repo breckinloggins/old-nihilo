c9ec8fe0191a3bcf21ccf15bff9d92ebee3960d0
Breckin Loggins <bloggins@greatfoundry.com>
2013-07-24 21:53:26 -0700

    Better names for stuff; start of IRQ handler

--- bootstrap.s
+++ bootstrap.s
@@ -1,6 +1,8 @@
 	.arch armv7-a
 	.fpu vfpv3
 	.text
+
+@ Prints the string pointed to by r0 to the UART
 	.global print_uart0
 print_uart0:
 	@ This routine allows us to print onto UART0DR
@@ -8,7 +10,7 @@ print_uart0:
 	ldrb	r3, [r0, #0]
 	cmp		r3, #0
 	bxeq	lr
-	ldr 	r2, =UART0DR
+	ldr 	r2, =UART0_ADR
 	ldr 	r2, [r2, #0]
 print_uart0_loop:
 	str		r3, [r2, #0]
@@ -17,18 +19,37 @@ print_uart0_loop:
 	bne		print_uart0_loop
 	bx 		lr
 
-	.global c_entry
+@ IRQ Handler
+	.global IRQ_Handler
+IRQ_Handler:
+	ldr 	r0, =IRQ_STRING
+	bl 		print_uart0
+	subs	pc, lr, #4
+
+@ MAIN - Where it all begins
+	.global nihilo_entry
 	@ This is the main entry point from startup.s
-c_entry:
-	ldr 	r0, =HELLO_WORLD
+nihilo_entry:
+	ldr 	r0, =WELCOME_STR
 	bl 		print_uart0
 	b 		.
 
+@ --- Versatile PB Memory Map ---
 	.section	.rodata
-	.global UART0DR
-UART0DR:
-	.word 	0x101F1000	@ This is the UART0 address on a versatileboard
-	.section 	.rodata.str1.4, "aMS",%progbits,1
 
-HELLO_WORLD: @ The string to print
+	.global PIC_ADR
+PIC_ADR:
+	.word 	0x10140000 	@ PrimeCell PL190 Vectored Interrupt Controller (PIC)
+
+	.global UART0_ADR
+UART0_ADR:
+	.word 	0x101F1000	@ PrimeCell PL011 UART 0
+
+@ -- Global Variables ---
+
+	.section 	.rodata.str1.4, "aMS",%progbits,1
+WELCOME_STR: @ The string to print
 	.ascii	"Nihilo\012\000"
+
+IRQ_STRING:
+	.ascii "IRQ\012\000"
--- startup.s
+++ startup.s
@@ -6,10 +6,10 @@ _Reset:
 	B .				/* Prefetch Abort	*/
 	B .				/* Data Abort		*/
 	B .				/* reserved			*/
-	B .				/* IRQ				*/
+	B IRQ_Handler	/* IRQ				*/
 	B .				/* FIQ				*/
 
 Reset_Handler:
 	LDR sp, =stack_top
-	BL c_entry
+	BL nihilo_entry
 	B .
