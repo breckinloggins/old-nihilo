aa7ac1a70a1d5ac87b1398f17d3cbdea1b87df68
Breckin Loggins <bloggins@greatfoundry.com>
2013-07-24 21:53:05 -0700

    Removing previously built file if it exists

--- nihilo.sh
+++ nihilo.sh
@@ -1,3 +1,5 @@
+rm bootstrap.bin 2> /dev/null
+
 TARGET=/Users/bloggins/bin/ndk/toolchains/arm-linux-androideabi-4.7/prebuilt/darwin-x86_64/bin/arm-linux-androideabi
 $TARGET-as -march=armv7-a startup.s -o startup.o
 $TARGET-as -march=armv7-a bootstrap.s -o bootstrap.o
